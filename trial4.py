ec6daa303905f6a0a4034510d582290f643f2edf
MWELU MUTINDA <carol.mweluh@gmail.com>
2019-10-30 12:52:32 +0300

    second commit

---
+++ trial4.py
@@ -0,0 +1,6 @@
+from sys import argv
+
+script, first, second =argv
+print("this is my first script:",script)
+print("this is my fiest variable:",first)
+print ("this is my second variable:",second)
